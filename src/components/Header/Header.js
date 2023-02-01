d4dd059d01898ee1c47f7f0eaff4763e5aabbae5
AyeshaKhan27 <111221860+AyeshaKhan27@users.noreply.github.com>
2023-02-01 19:59:45 +0500

    first commit by ayesha

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,49 @@
-import React from 'react'
-import profile from "../../Assets/Icons/profile.png"
+import React, { useState } from "react";
+import Profile from "../../Assets/Icons/profile.png"
 import "./header.css"
 const Header = () => {
-  return (
-    <div className='top'>
-        <div className="head">
-        <a href="">Details</a>
-        <a href="">Holdings</a>
-        <a href="">Pending Deal's</a>
-        <a href=""><img src={profile} alt="" /></a></div>
-    </div>
-  )
-}
+  const [activeTab, setActiveTab] = useState(0);
 
-export default Header
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+  };
+
+  return (
+    <div className="tabs">
+      <ul className="tabs__list">
+        <li
+          className={`tabs__item ${activeTab === 0 ? "tabs__item--active" : ""}`}
+          onClick={() => handleTabClick(0)}
+        >
+          Details
+        </li>
+        <li
+          className={`tabs__item ${activeTab === 1 ? "tabs__item--active" : ""}`}
+          onClick={() => handleTabClick(1)}
+        >
+          Holdings
+        </li>
+        <li
+          className={`tabs__item ${activeTab === 2 ? "tabs__item--active" : ""}`}
+          onClick={() => handleTabClick(2)}
+        >
+          Pending Deals 
+        </li>
+        <li
+          className={`tabs__item ${activeTab === 3 ? "tabs__item--active" : ""}`}
+          onClick={() => handleTabClick(3)}
+        >
+          <img src={Profile} alt="" />
+        </li>
+      </ul>
+      <div className="tabs__content">
+        {activeTab === 0 && <div>Content of Tab 1</div>}
+        {activeTab === 1 && <div>Content of Tab 2</div>}
+        {activeTab === 2 && <div>Content of Tab 3</div>}
+        {activeTab === 3 && <div>Content of Tab 4</div>}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
